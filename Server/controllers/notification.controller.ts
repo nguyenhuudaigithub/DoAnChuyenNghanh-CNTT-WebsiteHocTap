affe0072ebd5775472f6e4fbb3a8e8d125737b30
BVSoNxNB <buivanson09102002@gmail.com>
2023-10-31 02:19:07 +0700

    Delete read notifications -- with crone after a certain time

--- Server/controllers/notification.controller.ts
+++ Server/controllers/notification.controller.ts
@@ -4,15 +4,16 @@ import { CatchAsyncError } from "../middleware/catchAsyncErrors";
 import ErrorHandler from "../utils/ErrorHandler";
 import cron from "node-cron";
 
-// //delete notification -- only admin
-// cron.schedule("0 0 0 * * *", async () => {
-//   const thirtyDaysAgo = new Date(Date.now() - 30 * 24 * 60 * 60 * 1000);
-//   await NotificationModel.deleteMany({
-//     status: "read",
-//     createdAt: { $lt: thirtyDaysAgo },
-//   });
-//   console.log("Delete rea botifications");
-// });
+//delete notification -- only admin
+cron.schedule("0 0 0 * * *", async () => {
+  const thirtyDaysAgo = new Date(Date.now() - 30 * 24 * 60 * 60 * 1000);
+  await NotificationModel.deleteMany({
+    status: "read",
+    createdAt: { $lt: thirtyDaysAgo },
+  });
+  console.log("Delete rea botifications");
+});
+
 //  Nhận tất cả thông báo -- chỉ quản trị viên
 export const getNotifications = CatchAsyncError(
   async (req: Request, res: Response, next: NextFunction) => {
