708f3b1436066ea796a69412595a3946ddffa6ac
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2023-11-12 22:12:41 +0700

    fix4

--- client/app/admin/page.tsx
+++ client/app/admin/page.tsx
@@ -19,9 +19,9 @@ const page = (props: Props) => {
           <div className='1500px:w-[16%] w-1/5'>
             <AdminSidebar />
           </div>
-        </div>
-        <div className='w-[85%]'>
-          <DashboardHero isDashboard={true} />
+          <div className='w-[85%]'>
+            <DashboardHero isDashboard={true} />
+          </div>
         </div>
       </AdminProtected>
     </div>
