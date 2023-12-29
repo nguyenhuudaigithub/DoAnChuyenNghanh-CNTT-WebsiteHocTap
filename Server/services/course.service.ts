da6653a64e6bcdf50b4dc7cf4b8911c78e0d9b65
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2023-12-29 23:08:38 +0700

    test

--- Server/services/course.service.ts
+++ Server/services/course.service.ts
@@ -1,11 +1,13 @@
-import { Response } from "express";
-import CourseModel from "../models/course.model";
-import { CatchAsyncError } from "../middleware/catchAsyncErrors";
+import { Response } from 'express';
+import CourseModel from '../models/course.model';
+import { CatchAsyncError } from '../middleware/catchAsyncErrors';
+import { redis } from '../utils/redis';
 
 // create course
 export const createCourse = CatchAsyncError(
   async (data: any, res: Response) => {
     const course = await CourseModel.create(data);
+    await redis.set(course?._id, JSON.stringify(course));
     res.status(201).json({
       success: true,
       course,
